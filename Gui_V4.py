1aa9b8fdd15f9279e49aeffb69d1cbf488e6d3d3
Nc23147 <nc23147@email.school.nz>
2026-06-22 16:01:50 +1200

    22/6

--- Gui_V4.py
+++ Gui_V4.py
@@ -2,11 +2,17 @@ import tkinter as tk
 from tkinter import ttk, messagebox
 
 
+
+
+
+
 #functions
 def submit_item():
     name= name_entry.get().strip()
-    item_hired = item_entry.get().strip()
+    item_hired = selected_item.get().strip()
     items = number_hired_entry.get().strip()
+    recipt_num = recipt_entry.get().strip()
+
     if name== "":
         messagebox.showerror("Input Error", "Name cannot be blank")
         return
@@ -29,6 +35,9 @@ def submit_item():
         messagebox.showerror("Input Error","please enter an integer that is less than 500.")
         return
 
+
+
+
     print(name)
     print(item_hired)    
     print(items)
@@ -44,7 +53,7 @@ def submit_item():
 #Main window 
 root=tk.Tk()
 root.title("Party Hire Shop")
-root.geometry("800x200")
+root.geometry("500x200")
 
 #Main entry fields 
 tk.Label(root, text="Name:").grid(row = 0, column= 0, padx=10, pady=5, sticky="e")
@@ -55,30 +64,27 @@ tk.Label(root, text="Recipt Number:").grid(row = 1, column= 0, padx=10, pady=5, 
 recipt_entry = tk.Entry(root)
 recipt_entry.grid(row = 1, column= 1, padx=10, pady=5)
 
+#Dropdown
 tk.Label(root, text="Item hired:").grid(row = 2, column= 0, padx=10, pady=5, sticky="e")
-item_entry = tk.Entry(root)
-item_entry.grid(row = 2, column= 1, padx=10, pady=5)
+selected_item = ttk.Combobox(root, values=["Football","Basketball","Roadcone","Pineapple pen"], state="readonly")
+selected_item.grid(row = 2, column = 1)
+selected_item.current(0)
 
-tk.Label(root, text="Row #").grid(row = 2, column= 3, padx=10, pady=5, sticky="e")
-row_entry = tk.Entry(root)
-row_entry.grid(row = 2, column= 5, padx=10, pady=5)
+
+
 
 tk.Label(root, text="Number hired:").grid(row = 3, column= 0, padx=10, pady=5, sticky="e")
 number_hired_entry = tk.Entry(root)
 number_hired_entry.grid(row = 3, column= 1, padx=10, pady=5)
 
 #Buttons
-append_details_button = tk.Button(root, text="Append details")
-append_details_button.grid(row=0,column=3 , columnspan= 2, pady=10, padx=10)
 
 submit_details_button = tk.Button(root, text="Submit details", command= submit_item)
-submit_details_button.grid(row=0,column=4 , columnspan= 2, pady=10, padx=10)
+submit_details_button.grid(row=4,column=1 , columnspan= 1, pady=10)
 
 quit_button = tk.Button(root, text="Quit")
-quit_button.grid(row=0,column=6 , columnspan= 2, pady=10, padx=11)
+quit_button.grid(row=4,column=0 , columnspan= 1, pady=10)
 
-delete_row_button = tk.Button(root, text= "Delete Row")
-delete_row_button.grid(row= 2, column= 7,)
 
 
 
